089bd6c5f6fd7fe09956e79b8efa3c40ac6ef9fe
Thiago Vilela <thiagovilela@petasoft.com>
2017-03-05 20:09:56 -0300

    Commit #02 - Adicionando A3

--- src/com/exercicio/A.java
+++ src/com/exercicio/A.java
@@ -4,6 +4,7 @@ public class A {
 	
 	private int a1;
 	private float a2;
+	private String a3;
 	
 	public int getA1() {
 		return a1;
@@ -16,6 +17,12 @@ public class A {
 	}
 	public void setA2(float a2) {
 		this.a2 = a2;
+	}
+	public String getA3() {
+		return a3;
+	}
+	public void setA3(String a3) {
+		this.a3 = a3;
 	}
 	
 }
