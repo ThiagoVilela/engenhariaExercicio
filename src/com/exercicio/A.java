540da7331bf3fb911596ddcea8fada138ce932d2
Thiago Vilela <thiagovilela@petasoft.com>
2017-03-05 20:29:26 -0300

    Commit #05 - Removendo a4 do master

--- src/com/exercicio/A.java
+++ src/com/exercicio/A.java
@@ -5,7 +5,6 @@ public class A {
 	private int a1;
 	private float a2;
 	private String a3;
-	private String a4;
 	
 	//Métodos acessores
 	public int getA1() {
@@ -25,12 +24,6 @@ public class A {
 	}
 	public void setA3(String a3) {
 		this.a3 = a3;
-	}
-	public String getA4() {
-		return a4;
-	}
-	public void setA4(String a4) {
-		this.a4 = a4;
 	}
 	
 	//Métodos
