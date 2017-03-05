1756b1ae8a79340481f95025edab3cbdc3ff149c
Thiago Vilela <thiagovilela@petasoft.com>
2017-03-05 20:54:00 -0300

    Commit #08 - Adicionando classe b no master

---
+++ src/com/exercicio/B.java
@@ -0,0 +1,22 @@
+package com.exercicio;
+
+public class B {
+	//Atributos
+	private int b1;
+	private float b2;
+	
+	//Métodos acessores
+	public int getB1() {
+		return b1;
+	}
+	public void setB1(int b1) {
+		this.b1 = b1;
+	}
+	public float getB2() {
+		return b2;
+	}
+	public void setB2(float b2) {
+		this.b2 = b2;
+	}
+	
+}
